fa4ff341d460c410d510f27b99aa9f8467cf3186
LexBwmn <Abowman8556@icloud.com>
2024-08-28 13:45:39 -0400

    Base (Prototype)

---
+++ RandomChoice.py
@@ -0,0 +1,7 @@
+# program that will randomly generate starter coding project ideas 
+from random import choice
+
+# prototype projects 
+projects = ['Calculator', 'Scheduling Application', 'Cafe Shoppe'];
+
+print(choice(projects))
