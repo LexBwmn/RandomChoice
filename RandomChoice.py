ae4ca17dd0b8d343cf99c7a72c71c6941593fc12
LexBwmn <Abowman8556@icloud.com>
2024-08-28 16:31:58 -0400

    Fixed_API_Key

--- RandomChoice.py
+++ RandomChoice.py
@@ -1,7 +1,25 @@
 # program that will randomly generate starter coding project ideas 
-from random import choice
 
-# prototype projects 
-projects = ['Calculator', 'Scheduling Application', 'Cafe Shoppe']
+## BASE PROJECT PROTOTYPE##
 
-print(choice(projects))
+#from random import choice
+
+# prototype projects
+    # projects = ['Calculator', 'Scheduling Application', 'Cafe Shoppe']
+    # print(choice(projects))
+
+import openai
+import os
+
+#model that my chatbot is based off of 
+model = "gpt-4o-mini"
+
+
+def cont(messages, projects=0):
+    response = openai.chat.completions.create(
+        model=model,
+        messages=messages,
+        projects=projects,
+    )
+    return response.choices[0].message.content
+
